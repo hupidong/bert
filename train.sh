646005221eaf5966334678e48dd5a76824a12f49
suishitian <suishitian@git.com>
2021-03-01 19:23:21 +0800

    add train.sh, an example bash-command which shows how to fine-tuning

---
+++ train.sh
@@ -0,0 +1,17 @@
+export BERT_BASE_DIR='bert_tiny'
+export GLUE_DIR='glue_data'
+export TASK_NAME='CoLA'
+
+python run_classifier.py \
+	--task_name=$TASK_NAME \
+	--do_train=true \
+	--do_eval=true \
+	--data_dir=$GLUE_DIR/$TASK_NAME \
+	--vocab_file=$BERT_BASE_DIR/vocab.txt \
+	--bert_config_file=$BERT_BASE_DIR/bert_config.json \
+	--init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
+	--max_seq_length=128 \
+	--train_batch_size=32 \
+	--learning_rate=2e-5 \
+	--num_train_epochs=3.0 \
+	--output_dir=tmp/${TASK_NAME}_output/
