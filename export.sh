e4c35b8a96ac8f1d0f64feee79e1c7d4593d6e92
hupidong <hupidong@163.com>
2021-03-07 20:55:54 +0800

    update 'export.sh', transform ckpt model-file to pb file

--- export.sh
+++ export.sh
@@ -1,12 +1,13 @@
 export BERT_BASE_DIR='bert_tiny'
 export GLUE_DIR='glue_data'
 export TASK_NAME='CoLA'
+export TASK_NAME_L=$(echo $TASK_NAME | tr '[A-Z]' '[a-z]')
 
 python run_classifier.py \
 	--task_name=$TASK_NAME \
 	--do_train=false \
 	--do_eval=false \
-	--do_predict=true \
+	--do_predict=false \
 	--data_dir=$GLUE_DIR/$TASK_NAME \
 	--vocab_file=$BERT_BASE_DIR/vocab.txt \
 	--bert_config_file=$BERT_BASE_DIR/bert_config.json \
@@ -15,6 +16,6 @@ python run_classifier.py \
 	--train_batch_size=32 \
 	--learning_rate=2e-5 \
 	--num_train_epochs=3.0 \
-	--output_dir=tmp/${TASK_NAME}_output/ \
+	--output_dir=models/${TASK_NAME_L}/ \
 	--do_export=true \
-	--export_dir=exported/${TASK_NAME}
+	--export_dir=models/${TASK_NAME_L}/
