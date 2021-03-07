d8ef9adc810177a7ed4c3deea4b0bd33f199b268
hupidong <hupidong@163.com>
2021-03-07 20:54:28 +0800

    change ouput_dir of models

--- train.sh
+++ train.sh
@@ -1,6 +1,7 @@
 export BERT_BASE_DIR='bert_tiny'
 export GLUE_DIR='glue_data'
 export TASK_NAME='CoLA'
+export TASK_NAME_L=$(echo $TASK_NAME | tr '[A-Z]' '[a-z]')
 
 python run_classifier.py \
 	--task_name=$TASK_NAME \
@@ -14,4 +15,4 @@ python run_classifier.py \
 	--train_batch_size=32 \
 	--learning_rate=2e-5 \
 	--num_train_epochs=3.0 \
-	--output_dir=tmp/${TASK_NAME}_output/
+	--output_dir=models/${TASK_NAME_L}/
