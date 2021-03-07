6e02553a79d7346eecbe3b6c3057f44b4192db3f
hupidong <hupidong@163.com>
2021-03-07 21:07:48 +0800

    add 'tfserving_with_model_config_file.sh' which run docker service using tensorflow serving with '--model_config_file' parameter

---
+++ tfserving_with_model_config_file.sh
@@ -0,0 +1,8 @@
+docker run -ti \
+    --name cola \
+    -p 8809:8809 \
+    --mount type=bind,source=/Users/hupidong/Work/Learning/bert/bert/models,target=/home/models/bert tensorflow/serving \
+    --entrypoint=tensorflow_model_server \
+    --rest_api_port=8809 \
+    --model_config_file=/home/models/bert/model.config \
+    --allow_version_labels_for_unavailable_models=true
